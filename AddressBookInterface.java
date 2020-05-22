02b82e7df7b3b0b4961de7aad4ee3ddb4a61cf0f
umeshhpatill <54363666+umeshhpatill@users.noreply.github.com>
2020-05-22 16:56:46 +0530

    Address book interface

---
+++ AddressBookInterface.java
@@ -0,0 +1,19 @@
+package addressbook;
+
+public interface AddressBookInterface {
+	
+	public Person addPerson();
+	
+	public void editPerson();
+	
+	public void deletePerson();
+	
+	public void sortByName();
+	
+	public void sortByZip();
+	
+	public void dispaly();
+
+	
+
+}
