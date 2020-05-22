3555fdee65183009d884733b5bf7fc456a51f7e9
umeshhpatill <54363666+umeshhpatill@users.noreply.github.com>
2020-05-22 16:54:15 +0530

    Address book manager interface

---
+++ AddressBookManagerInterface.java
@@ -0,0 +1,22 @@
+package addressbook;
+
+public interface AddressBookManagerInterface {
+	
+public void newAddressBook();
+	
+	public void openAddressBook();
+	
+	public void saveAddressBook();
+	
+	public void saveAsAddressBook();
+	
+	public void closeAddressBook();
+	
+	public void quit();
+}
+	
+	
+		
+
+
+
